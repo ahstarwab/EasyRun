28c8e174a89810d5cc1a873232afeb57ac5ec688
Juhun Kim <johnjhkim@hanmail.net>
2018-04-06 10:29:12 +0900

    Test for executing other apps

--- EasyRun/ViewController.swift
+++ EasyRun/ViewController.swift
@@ -20,6 +20,49 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func doExecuteOtherApp(_ sender: UIButton) {
+        //openKakao()
+        openMaps()
+        //openMessenger()
+        //openGoogleWebsite()
+    }
+    
+    func openKakao() {
+        let kakaoStory = "kakaostory://"
+        let kakaoStoryURL = NSURL(string: kakaoStory)
+        
+        if UIApplication.shared.canOpenURL(kakaoStoryURL! as URL) {
+            UIApplication.shared.openURL(kakaoStoryURL! as URL)
+        }
+        else {
+            print("No kakaostory installed.")
+        }
+    }
+    
+    func openMessenger(){
+        let myUrl = "sms:"
+        
+        if let url = URL(string: "\(myUrl)") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    
+    func openMaps() {
+        //let myUrl = "http://maps.apple.com/?q=newyork"
+        //let myUrl = "http://maps.apple.com/?q"
+        let myUrl = "comapplemaps://"
+        
+        if let url = URL(string: "\(myUrl)") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    
+    func openGoogleWebsite(){
+        let myUrl = "http://www.google.com"
+        
+        if let url = URL(string: "\(myUrl)") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
 }
 
