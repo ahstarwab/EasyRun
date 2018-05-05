80339635f7e689f097b3081cc1f47e1d8c3f2600
Juhun Kim <johnjhkim@hanmail.net>
2018-05-05 20:53:29 +0900

    ThirdViewController

--- EasyRun/ThirdViewController.swift
+++ EasyRun/ThirdViewController.swift
@@ -24,6 +24,7 @@ class ThirdViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+      
     }
 
     override func didReceiveMemoryWarning() {
