1bb40b9c893e144ff943a69a97df74a3376500bb
Juhun Kim <johnjhkim@hanmail.net>
2018-05-11 21:27:58 +0900

    add patterns

--- EasyRun/ThirdViewController.swift
+++ EasyRun/ThirdViewController.swift
@@ -9,16 +9,11 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
-
     
-    @IBOutlet var imgView: UIImageView!
+    @IBAction func Pattern1(_ sender: UIButton) {
+        
+    }
     
-    
-    var lastPoint: CGPoint!
-    var lineSize:CGFloat = 5.0
-    var lineColor = UIColor.gray.cgColor
-    var touchcount = 0
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,64 +26,5 @@ class ThirdViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touch = touches.first! as UITouch
-        
-        lastPoint = touch.location(in: imgView)
-    }
-    
-    
-    
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        UIGraphicsBeginImageContext(imgView.frame.size)
-        UIGraphicsGetCurrentContext()?.setStrokeColor(lineColor)
-        UIGraphicsGetCurrentContext()?.setLineCap(CGLineCap.round)
-        UIGraphicsGetCurrentContext()?.setLineWidth(lineSize)
-        
-        let touch = touches.first! as UITouch
-        let currPoint = touch.location(in: imgView)
-        
-        imgView.image?.draw(in: CGRect(x:0, y:0, width: imgView.frame.size.width,
-                                       height: imgView.frame.size.height))
-        UIGraphicsGetCurrentContext()?.move(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
-        UIGraphicsGetCurrentContext()?.addLine(to: CGPoint(x: currPoint.x, y: currPoint.y))
-        UIGraphicsGetCurrentContext()?.strokePath()
-        
-        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        lastPoint = currPoint
-    }
-    
-    
-    
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        UIGraphicsBeginImageContext(imgView.frame.size)
-        UIGraphicsGetCurrentContext()?.setStrokeColor(lineColor)
-        UIGraphicsGetCurrentContext()?.setLineCap(CGLineCap.round)
-        UIGraphicsGetCurrentContext()?.setLineWidth(lineSize)
-        
-        imgView.image?.draw(in: CGRect(x:0, y:0, width: imgView.frame.size.width,
-                                       height: imgView.frame.size.height))
-        UIGraphicsGetCurrentContext()?.move(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
-        UIGraphicsGetCurrentContext()?.addLine(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
-        UIGraphicsGetCurrentContext()?.strokePath()
-        
-        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        touchcount += 1
-        
-        if touchcount == 2{
-            imgView.image = nil
-            touchcount = 0
-        }
-    }
-    
     
 }
